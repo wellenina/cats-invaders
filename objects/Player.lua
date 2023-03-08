5aee309a912f4b93222e86a867823d6649097e82
wellenina <giulia.fanzaghi@gmail.com>
2023-03-08 21:46:48 +0100

    Add new players

--- objects/Player.lua
+++ objects/Player.lua
@@ -1,5 +1,20 @@
-local PLAYER_SPRITE_WIDTH, PLAYER_SPRITE_HEIGHT = 180, 30
-local PLAYER_QUAD_WIDTH, PLAYER_QUAD_HEIGHT = 30, 30
+-- players
+local PLAYERS_SPRITE_WIDTH, PLAYERS_SPRITE_HEIGHT = 192, 384
+local PLAYER_QUAD_WIDTH, PLAYER_QUAD_HEIGHT = 32, 32
+local PLAYERS = PLAYERS_SPRITE_HEIGHT / PLAYER_QUAD_HEIGHT
+local PLAYER_FRAMES = PLAYERS_SPRITE_WIDTH / PLAYER_QUAD_WIDTH
+
+local playersQuads = {}
+for i = 1, PLAYERS, 1 do
+    local quads = {}
+    for j = 1, PLAYER_FRAMES, 1 do
+        table.insert(quads, love.graphics.newQuad(PLAYER_QUAD_WIDTH * (j-1), PLAYER_QUAD_HEIGHT * (i-1),
+        PLAYER_QUAD_WIDTH, PLAYER_QUAD_HEIGHT, PLAYERS_SPRITE_WIDTH, PLAYERS_SPRITE_HEIGHT))
+    end
+    table.insert(playersQuads, quads)
+end
+
+playerWidth, playerHeight = PLAYER_QUAD_WIDTH - 2, PLAYER_QUAD_HEIGHT - 2
 
 local FRAME_DURATION = 0.2
 local frameTimer = 0
@@ -9,37 +24,32 @@ local flickerTimer = 0
 
 local PLAYER_SPEED = 200
 
+-- bullets
 local PLAYER_BULLET_SPRITE_WIDTH, PLAYER_BULLET_SPRITE_HEIGHT = 84, 10
 local BULLET_QUAD_WIDTH, BULLET_QUAD_HEIGHT = 6, 10
 
+playerBulletQuads = {} -- 14 bullets
+for i = 1, PLAYER_BULLET_SPRITE_WIDTH / BULLET_QUAD_WIDTH, 1 do
+    table.insert(playerBulletQuads, love.graphics.newQuad(BULLET_QUAD_WIDTH * (i-1), 0,
+    BULLET_QUAD_WIDTH, BULLET_QUAD_HEIGHT, PLAYER_BULLET_SPRITE_WIDTH, PLAYER_BULLET_SPRITE_HEIGHT))
+end
+
 local PLAYER_BULLETS_LIMIT = 1
+
 
 Player = {
 
     load = function()
+        playersSprite = love.graphics.newImage('images/players-spritesheet.png')
+        playerBulletSprite = love.graphics.newImage('images/player-bullets-spritesheet.png')
+        
+        playerX, playerY = VIRTUAL_WIDTH / 2 - playerWidth / 2, VIRTUAL_HEIGHT - playerHeight - 20
 
-        playerSprite = love.graphics.newImage('images/main-character-spritesheet.png')
-        playerQuads = {}
-        for i = 1, PLAYER_SPRITE_WIDTH / PLAYER_QUAD_WIDTH, 1 do
-            table.insert(playerQuads, 
-                love.graphics.newQuad(PLAYER_QUAD_WIDTH * (i-1), 0, PLAYER_QUAD_WIDTH, PLAYER_QUAD_HEIGHT, PLAYER_SPRITE_WIDTH, PLAYER_SPRITE_HEIGHT))
-        end
-
-        playerWidth, playerHeight = PLAYER_QUAD_WIDTH, PLAYER_QUAD_HEIGHT
-        playerX = VIRTUAL_WIDTH / 2 - playerWidth / 2
-        playerY = VIRTUAL_HEIGHT - playerHeight - 20
-
+        selectedPlayer = 2
         playerFrame = 1
         playerScale = 1
 
         isInvulnerable = false
-
-        playerBulletSprite = love.graphics.newImage('images/player-bullets-spritesheet.png')
-        playerBulletQuads = {} -- 14 bullets
-        for i = 1, PLAYER_BULLET_SPRITE_WIDTH / BULLET_QUAD_WIDTH, 1 do
-            table.insert(playerBulletQuads, 
-                love.graphics.newQuad(BULLET_QUAD_WIDTH * (i-1), 0, BULLET_QUAD_WIDTH, BULLET_QUAD_HEIGHT, PLAYER_BULLET_SPRITE_WIDTH, PLAYER_BULLET_SPRITE_HEIGHT))
-        end
     end,
 
     flicker = function(__self, dt)
@@ -77,7 +87,7 @@ Player = {
     end,
 
     render = function()
-        love.graphics.draw(playerSprite, playerQuads[playerFrame], playerX, playerY, 0, playerScale)
+        love.graphics.draw(playersSprite, playersQuads[selectedPlayer][playerFrame], playerX, playerY, 0, playerScale)
     end
 
 }
